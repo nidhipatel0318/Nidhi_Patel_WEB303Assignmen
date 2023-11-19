6665ec76ea7d9437675f0ba645e04048ed865d66
nidhipatel0318 <135552374+nidhipatel0318@users.noreply.github.com>
2023-11-19 16:13:03 -0500

    Add files via upload

--- WEB303As08/js/script.js
+++ WEB303As08/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $.ajax({
         url: 'characters.json',
-
         dataType: 'json',
         success: function(data) {
             var $table = $('#characterTable tbody');
@@ -17,6 +16,7 @@ $(document).ready(function() {
                 $row.append('<td>' + character.team + '</td>');
                 $row.append('<td>' + character.occupation + '</td>');
                 $row.append('<td>' + character.description + '</td>');
+                $row.append('<td>' + character.birthdate + '</td>'); // Display birthdate
                 $table.append($row);
 
                 var lastNameFirstChar = character.lastName.charAt(0).toUpperCase();
@@ -28,6 +28,33 @@ $(document).ready(function() {
             });
 
             updateFilterCounts();
+
+            $('th').click(function() {
+                var column = $(this).index();
+                var sortDirection = $(this).attr('data-sort') || 'asc';
+
+                var sortedRows = $table.find('tr').toArray().sort(function(a, b) {
+                    var aValue = $(a).find('td').eq(column).text();
+                    var bValue = $(b).find('td').eq(column).text();
+
+                    if (column === 3) {
+                        aValue = parseInt(aValue, 10);
+                        bValue = parseInt(bValue, 10);
+                    }
+
+                    return (sortDirection === 'asc') ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+                });
+
+                sortDirection = (sortDirection === 'asc') ? 'desc' : 'asc';
+                $(this).attr('data-sort', sortDirection);
+
+                $('th').not(this).removeAttr('data-sort').find('span').remove();
+
+                var chevron = (sortDirection === 'asc') ? '&#x25B2;' : '&#x25BC;';
+                $(this).append('<span style="float: right;">' + chevron + '</span>');
+
+                $table.empty().append(sortedRows);
+            });
 
             $('#search').on('input', function() {
                 var searchTerm = $(this).val().toLowerCase();
